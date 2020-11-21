c56982f625fecba1b6b8a4949ebdfe093f92d996
vladementei@gmail.com <Pravda1650>
2020-11-21 13:42:56 +0300

    feat: app component

--- web-ui/src/app/app.component.ts
+++ web-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {RestService} from './core/services/rest-service.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'web-ui';
+
+  imageSrc: string;
+  selectedFile: File;
+
+  constructor(private restService: RestService) {
+  }
+
+  previewFile(file: File): void {
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (() => {
+        this.imageSrc = reader.result.toString();
+      });
+      reader.readAsDataURL(file);
+    }
+    this.selectedFile = file;
+  }
+
+  uploadImage() {
+    this.restService.sendImage(this.selectedFile).subscribe();
+  }
 }
