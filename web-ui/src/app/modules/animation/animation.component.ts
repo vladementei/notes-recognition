d70058c391f30ab5ba0c995db63fbfe339cd79b9
vladementei@gmail.com <Pravda1650>
2020-11-22 12:36:39 +0300

    feat: run animation modules

--- web-ui/src/app/modules/animation/animation.component.ts
+++ web-ui/src/app/modules/animation/animation.component.ts
@@ -1,5 +1,6 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import {RxUnsubscribe} from '../../core/services/rx-unsubscribe';
+import abcjs from 'abcjs';
 
 @Component({
   selector: 'animation',
@@ -7,9 +8,38 @@ import {RxUnsubscribe} from '../../core/services/rx-unsubscribe';
   styleUrls: ['./animation.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AnimationComponent extends RxUnsubscribe {
+export class AnimationComponent extends RxUnsubscribe implements OnInit {
 
-  constructor() {
+  @ViewChild('paper') paperView;
+  abc_editor;
+  isAnimationWorks: boolean = undefined;
+
+  constructor(private cdr: ChangeDetectorRef) {
     super();
+  }
+
+  ngOnInit(): void {
+    this.cdr.detectChanges();
+    this.abc_editor = new abcjs.Editor(
+      'abc', {
+        paper_id: 'paper',
+        warnings_id: 'warnings',
+        abcjsParams: {
+          add_classes: true
+        }
+      }
+    );
+  }
+
+  animate(): void {
+    let params = {showCursor: true};
+    let paper = this.paperView.nativeElement;
+    abcjs.startAnimation(paper, this.abc_editor.tunes[0], params);
+    this.isAnimationWorks = true;
+  }
+
+  pause(): void {
+    abcjs.pauseAnimation(this.isAnimationWorks);
+    this.isAnimationWorks = !this.isAnimationWorks;
   }
 }
