dd5defcccc00ba485c9aa054440a4606ba9b62c4
vladementei@gmail.com <Pravda1650>
2020-12-03 17:54:19 +0300

    feat: add autoscroller

--- web-ui/src/app/modules/animation/animation.component.ts
+++ web-ui/src/app/modules/animation/animation.component.ts
@@ -15,6 +15,7 @@ export class AnimationComponent extends RxUnsubscribe implements OnInit {
   musicEditor;
   isAnimationWorks: boolean = undefined;
   music: string;
+  isMobileView: boolean = false;
 
 
   constructor(private cdr: ChangeDetectorRef) {
@@ -22,26 +23,32 @@ export class AnimationComponent extends RxUnsubscribe implements OnInit {
   }
 
   ngOnInit(): void {
-    this.music = 'G4 c4 d2 d2 d2 B2 A4 A4 A4 d4' +
-      ' d2 e2 e2 c2 B4 G4 B4 e4 e2 f2 e2 e2' +
-      ' B4 G2 G2 d2 B4 c4' +
-      ' c4 c2 d2 c2 B2 A4 A4 A4 d4' +
-      ' d2 e2 d2 c2 B2 G2 B2 e4 e2 f2 e2 d2' +
-      ' d4 A4 G2 G2 A4 d2 B4 c4';
+    this.music = this.updateMusicSheetToView('G4 c4 d2 d2 d2 B2 A4 A4 A4 d4 |\n' +
+      ' d2 e2 e2 c2 B4 G4 B4 e4 e2 f2 e2 e2 |\n' +
+      ' B4 G2 G2 d2 B4 c4 |\n' +
+      ' c4 c2 d2 c2 B2 A4 A4 A4 d4 |\n' +
+      ' d2 e2 d2 c2 B2 G2 B2 e4 e2 f2 e2 d2 |\n' +
+      ' d4 A4 G2 G2 A4 d2 B4 c4 |');
     this.cdr.detectChanges();
 
+    const abcjsParams: any = {
+      add_classes: true,
+    }
+    if (this.isMobileView) {
+      abcjsParams.staffwidth = 80000;
+    }
     this.musicEditor = new abcjs.Editor(
-      'abc', {
+      'musicInput', {
         paper_id: 'musicSheet',
         warnings_id: 'warnings',
-        abcjsParams: {
-          add_classes: true,
-          staffwidth: 80000,
-        }
+        abcjsParams: abcjsParams
       }
     );
 
-    this.musicSheetWidth = this.getMusicSheetWidth();
+    if (this.isMobileView) {
+      this.updateMusicSheetWidth();
+    }
+
     this.cdr.detectChanges();
   }
 
@@ -50,6 +57,14 @@ export class AnimationComponent extends RxUnsubscribe implements OnInit {
     const sheet = this.musicSheet.nativeElement;
     abcjs.startAnimation(sheet, this.musicEditor.tunes[0], params);
     this.isAnimationWorks = true;
+
+    if (this.isMobileView) {
+      const cursor = document.getElementsByClassName('abcjs-cursor cursor')[0];
+      const int = window.setInterval(function() {
+        cursor.scrollIntoView({behavior: 'smooth', inline: "start"})
+      }, 1000);
+      //setTimeout(() => clearInterval(int), 10000);
+    }
   }
 
   pause(): void {
@@ -57,13 +72,21 @@ export class AnimationComponent extends RxUnsubscribe implements OnInit {
     this.isAnimationWorks = !this.isAnimationWorks;
   }
 
-  getMusicSheetWidth(): number {
-    return Math.floor((this.musicSheet?.nativeElement?.firstChild?.lastChild?.x?.baseVal?.value || this.musicSheetWidth) + 40);
+  updateMusicSheetWidth(): void {
+    const width: number = Math.floor((this.musicSheet?.nativeElement?.firstChild?.lastChild?.x?.baseVal?.value || this.musicSheetWidth) + 40);
+    if (this.musicSheet?.nativeElement?.firstChild?.width?.baseVal?.value && width) {
+      this.musicSheet.nativeElement.firstChild.width.baseVal.value = width;
+      this.musicSheetWidth = width;
+    }
+  }
+
+  updateMusicSheetToView(music: string): string {
+    return this.isMobileView ? music.replace(/[\|\n]/g, '') : music;
   }
 
   onMusicChange() {
     this.cdr.detectChanges();
-    this.musicSheetWidth = this.getMusicSheetWidth();
+    this.updateMusicSheetWidth();
     this.cdr.detectChanges();
   }
 }
