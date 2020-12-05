0eb5b4eb39d41e7011391f8f9b38d0731605ab2f
vladementei@gmail.com <Pravda1650>
2020-12-05 15:50:46 +0300

    feat: main page navigator

--- web-ui/src/app/modules/main/main.component.ts
+++ web-ui/src/app/modules/main/main.component.ts
@@ -1,5 +1,8 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {RxUnsubscribe} from '../../core/services/rx-unsubscribe';
+import {Router} from '@angular/router';
+import {NavLink} from '../../models/nav-link.model';
+import {NAV_LINKS} from '../../constants/navigation.constants';
 
 @Component({
   selector: 'main',
@@ -9,7 +12,17 @@ import {RxUnsubscribe} from '../../core/services/rx-unsubscribe';
 })
 export class MainComponent extends RxUnsubscribe {
 
-  constructor() {
+  readonly NAV_LINKS = NAV_LINKS;
+
+  constructor(private router: Router,) {
     super();
+  }
+
+  navigate(link: NavLink): void {
+    if (link.href) {
+      window.open(link.href, '_blank');
+    } else if (link.route) {
+      this.router.navigate([link.route]);
+    }
   }
 }
