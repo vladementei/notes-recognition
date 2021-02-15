70a6b529c0081c67716f7e8dd483f63f9a6ddabf
vladementei@gmail.com <Pravda1650>
2021-02-15 18:19:18 +0300

    feat: gateway index.ts

--- gateway/index.ts
+++ gateway/index.ts
@@ -1,8 +1,23 @@
-import express from 'express';
-// rest of the code remains same
-const app = express();
-const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript'));
-app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+import express from "express";
+import bodyParser from "body-parser";
+import {Request, Response, NextFunction} from "express-serve-static-core";
+
+const app = express(),
+    port = process.env.NODEJS_PORT || 8080,
+    root = "/";
+
+const allowCrossDomain = (req: Request, res: Response, next: NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+};
+
+app.use(allowCrossDomain);
+app.use(bodyParser.json())
+
+app.get(root, (req, res) => res.send('Notes recognition gateway!'));
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
