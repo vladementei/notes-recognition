a143f7075a343a16121266915ff0f2abadb2e3eb
vladementei@gmail.com <Pravda1650>
2021-02-28 11:43:00 +0300

    feat: generate json web tokens

--- idp/src/controller/authorization.controller.ts
+++ idp/src/controller/authorization.controller.ts
@@ -3,10 +3,21 @@ import "reflect-metadata";
 import {removeCors} from "../middleware";
 import {AppRoutes} from "../constants";
 const fs = require("fs");
+import * as jwt from "jsonwebtoken";
+import {SignOptions} from "jsonwebtoken";
 
 class AuthorizationRoutes {
     public static readonly PUBLIC_KEY = "public-key";
+    public static readonly TOKEN = "token";
 }
+
+const signOptions: SignOptions = {
+    issuer: "Otvinta corp",
+    subject: "otvinta@gmail.com",
+    audience: "http://localhost",
+    expiresIn: "24h",
+    algorithm: "RS256"
+};
 
 @Controller()
 @UseBefore(removeCors)
@@ -20,6 +31,25 @@ export class AuthorizationController {
                     return;
                 }
                 resolve(data);
+            });
+        });
+    }
+
+    @Get(`${AppRoutes.AUTHORIZATION}/${AuthorizationRoutes.TOKEN}`)
+    getToken() {
+        return new Promise((resolve, reject) => {
+            fs.readFile("assets/private.key", "utf8", (err: NodeJS.ErrnoException, data: string) => {
+                if (err) {
+                    reject(new HttpError(400, err.message));
+                    return;
+                }
+
+                const payload = {
+                    role: "Anonymous"
+                };
+
+                const token = jwt.sign(payload, data, signOptions);
+                resolve(token);
             });
         });
     }
