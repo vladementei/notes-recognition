17273c20aefa46c97753a3b6b24cf3dc3da271dc
vladementei@gmail.com <Pravda1650>
2021-03-08 11:39:45 +0300

    feat: login mechanism

--- idp/src/controller/authorization.controller.ts
+++ idp/src/controller/authorization.controller.ts
@@ -1,14 +1,18 @@
-import {Controller, Get, HttpError, UseBefore} from "routing-controllers"
+import {Body, Controller, Get, HttpError, Post, UseBefore} from "routing-controllers"
 import "reflect-metadata";
 import {removeCors} from "../middleware";
 import {AppRoutes} from "../constants";
 const fs = require("fs");
 import * as jwt from "jsonwebtoken";
 import {SignOptions} from "jsonwebtoken";
+import {User} from "../model";
+import axios from "axios";
+
 
 class AuthorizationRoutes {
     public static readonly PUBLIC_KEY = "public-key";
     public static readonly TOKEN = "token";
+    public static readonly LOGIN = "login";
 }
 
 const signOptions: SignOptions = {
@@ -37,17 +41,34 @@ export class AuthorizationController {
 
     @Get(`${AppRoutes.AUTHORIZATION}/${AuthorizationRoutes.TOKEN}`)
     getToken() {
+        const payload = {
+            role: "Anonymous"
+        };
+        return this.generateToken(payload);
+    }
+
+    @Post(`${AppRoutes.AUTHORIZATION}/${AuthorizationRoutes.LOGIN}`)
+    login(@Body() user: User) {
+        return new Promise((resolve, reject) => {
+            resolve(axios.get(`http://localhost:3000/users?username=${user.username}`)
+                .then(response => {
+                    if (response.data?.length && response.data[0].password === user.password) {
+                        return this.generateToken(response.data[0]);
+                    } else {
+                        throw new HttpError(404, "Login failed");
+                    }
+                })
+            );
+        });
+    }
+
+    private generateToken(payload: object): Promise<unknown> {
         return new Promise((resolve, reject) => {
             fs.readFile("assets/private.key", "utf8", (err: NodeJS.ErrnoException, data: string) => {
                 if (err) {
                     reject(new HttpError(400, err.message));
                     return;
                 }
-
-                const payload = {
-                    role: "Anonymous"
-                };
-
                 const token = jwt.sign(payload, data, signOptions);
                 resolve(token);
             });
